e1b301de63325baed928fbab56e9332168127b15
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2020-08-13 15:15:23 +0200

    ApiError public

--- Sources/InfomaniakLogin/ApiError.swift
+++ Sources/InfomaniakLogin/ApiError.swift
@@ -1,16 +1,16 @@
 //
-//  File.swift
-//  
+//  ApiError.swift
+//
 //
 //  Created by Philippe Weidmann on 13.08.20.
 //
 
 import Foundation
 
-class ApiError: Codable {
+@objc public class ApiError: NSObject, Codable {
     let error: String
     let errorDescription: String?
-    
+
     enum CodingKeys: String, CodingKey {
         case error
         case errorDescription = "error_description"
