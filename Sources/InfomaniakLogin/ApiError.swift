88f59be47da2b5028b7d6f534823e5c5a79f40dc
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2020-08-13 15:22:44 +0200

    ApiError pubic fields

--- Sources/InfomaniakLogin/ApiError.swift
+++ Sources/InfomaniakLogin/ApiError.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 @objc public class ApiError: NSObject, Codable {
-    let error: String
-    let errorDescription: String?
+    @objc public let error: String
+    @objc public let errorDescription: String?
 
     enum CodingKeys: String, CodingKey {
         case error
