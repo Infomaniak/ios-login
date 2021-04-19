5b124560d4542c37b4a4e8c18190839cf8750aea
Florentin Bekier <florentin.bekier@infomaniak.com>
2021-04-19 11:23:51 +0200

    Add comment header
    Signed-off-by: Florentin Bekier <florentin.bekier@infomaniak.com>

--- Sources/InfomaniakLogin/WebViewController.swift
+++ Sources/InfomaniakLogin/WebViewController.swift
@@ -1,10 +1,18 @@
-//
-//  WebViewController.swift
-//  InfomaniakLogin
-//
-//  Created by Ambroise Decouttere on 02.06.2020.
-//  Copyright © 2020 Infomaniak. All rights reserved.
-//
+/*
+Copyright 2020 Infomaniak Network SA
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 import UIKit
 import WebKit
@@ -74,15 +82,15 @@ class WebViewController: UIViewController, WKUIDelegate {
         self.title = navBarTitle ?? "login.infomaniak.com"
 
         if #available(iOS 13.0, *) {
-            let navigationAppaerance = UINavigationBarAppearance()
-            navigationAppaerance.configureWithDefaultBackground()
+            let navigationAppearance = UINavigationBarAppearance()
+            navigationAppearance.configureWithDefaultBackground()
             if let navBarColor = navBarColor {
-                navigationAppaerance.backgroundColor = navBarColor
+                navigationAppearance.backgroundColor = navBarColor
             }
             if let navBarTitleColor = navBarTitleColor {
-                navigationAppaerance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: navBarTitleColor]
+                navigationAppearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: navBarTitleColor]
             }
-            self.navigationController?.navigationBar.standardAppearance = navigationAppaerance
+            self.navigationController?.navigationBar.standardAppearance = navigationAppearance
         } else {
             if let navBarColor = navBarColor {
                 self.navigationController?.navigationBar.backgroundColor = navBarColor
--- login-Example/login-Example/ViewController.swift
+++ login-Example/login-Example/ViewController.swift
@@ -1,10 +1,18 @@
-//
-//  ViewController.swift
-//  login-Example
-//
-//  Created by Ambroise Decouttere on 27/05/2020.
-//  Copyright © 2020 infomaniak. All rights reserved.
-//
+/*
+Copyright 2020 Infomaniak Network SA
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 import UIKit
 import InfomaniakLogin
@@ -30,7 +38,7 @@ class ViewController: UIViewController, InfomaniakLoginDelegate {
 
     func showError(error: String) {
         let alertController = UIAlertController(title: error, message:
-        nil, preferredStyle: .alert)
+                nil, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "Fermer", style: .default, handler: {
             _ in
             self.dismiss(animated: true, completion: nil)
@@ -39,4 +47,3 @@ class ViewController: UIViewController, InfomaniakLoginDelegate {
     }
 
 }
-
